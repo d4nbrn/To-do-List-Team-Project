65bef0bd6373d9cfcb9d194042cffb0d27541282
d4nbrn <vmzr0073@leeds.ac.uk>
2025-11-06 10:45:52 +0000

    Near end of 2nd function

--- addDeleteFromList.py
+++ addDeleteFromList.py
@@ -4,7 +4,7 @@ def addDeleteFromList(list_):
     displaylist = viewContents(list_)
     print(f"Currently the list looks like this: {displaylist}")
     time.sleep(2)
-    addOrDelete = int(input("Would you like to 1. Add or 2. Delete - Please input either 1 or 2")).strip()
+    addOrDelete = int(input("Would you like to 1. Add or 2. Delete \n Please input either 1 or 2\n"))
     if addOrDelete == 1:
         listContents = input("Please input the task you would like to add to the list")
         listContents = str(len(list_)+1) +"." + listContents
@@ -12,15 +12,17 @@ def addDeleteFromList(list_):
         list_.append([listContents,priorityLevel])
         displaylist = viewContents(list_)
         print(f"Currently the list looks like this: {displaylist}")
-    elif: addOrDelete == 2:
+    elif addOrDelete == 2:
         foundElement = False
+        i = 0
         elementToDelete = int(input("Enter the task number that you would like to delete"))
-        for i in range(len(list_)-1):
+        while foundElement == False:
             if list_[i][0][0] == elementToDelete:
                 list_.pop(i)
                 foundElement = True
             else:
-                foundElement = False
+                i += 1
+            foundElement = False
         if foundElement == True:
             print("The element was successfully deleted")
         else:
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from viewContents import viewContents
 from addDeleteFromList import addDeleteFromList
 toDolist = [["1. Do the washing",2],["2. Do the drying",1], ["3. Fill up the car",3]]
 try:
-    continue_ = int(input("Welcome to the To-Do list /n There are several options you can choose from, \n 1. View the To Do list \n 2. Add/Delete from the to do list \n 3. Make changes to elements of the to do list"))
+    continue_ = int(input("Welcome to the To-Do list \n There are several options you can choose from, \n 1. View the To Do list \n 2. Add/Delete from the to do list \n 3. Make changes to elements of the to do list"))
 except ValueError:
     print("Please input an integer between 1-3")
 if continue_ == 1:
